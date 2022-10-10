2e9930b9960c7d88a3a73e129e06a08185b17e81
Francis Therien <frtherien@gmail.com>
2022-10-10 16:43:26 -0400

    Consolidate and rename test files

--- tests/test_svdparser.nim
+++ tests/test_svdparser.nim
@@ -1,7 +1,10 @@
-import unittest
-import svdparser
-import basetypes
-import utils
+import std/unittest
+import std/sequtils
+import std/strutils
+import ../svdparser
+import ../expansions
+import ../basetypes
+import ../utils
 
 # Test suites
 
@@ -155,3 +158,72 @@ suite "Parser Tests":
 
       pmux1.dimGroup.dim.get == 16
       pmux1.dimGroup.dimIncrement.get == 0x1
+
+
+func derived(dev: SvdDevice): SvdDevice =
+  result = dev.deepCopy
+  result.peripherals.expandDerives
+
+suite "Derivations and expansions":
+  setup:
+    let
+      device {.used.} = readSVD("./tests/ARM_Example.svd").derived
+      samd21 {.used.} = readSVD("./tests/ATSAMD21G18A.svd").derived
+
+  test "Peripheral derived":
+    let
+      timer0 = device.getPeriphByName("TIMER0")
+      timer1 = device.getPeriphByName("TIMER1")
+      timer2 = device.getPeriphByName("TIMER2")
+      port_iobus = samd21.getPeriphByName("PORT_IOBUS")
+
+    check:
+      timer1.baseAddress == 0x40010100
+      timer1.interrupts.len == 1
+      timer1.interrupts[0].name == "TIMER1"
+      timer1.registers.len == timer0.registers.len
+      timer1.nimTypeName == timer0.nimTypeName
+
+      timer2.baseAddress == 0x40010200
+      timer2.interrupts.len == 1
+      timer2.interrupts[0].name == "TIMER2"
+      timer2.registers.len == timer0.registers.len
+      timer2.nimTypeName == timer0.nimTypeName
+
+      port_iobus.prependToName.get == "PORT_IOBUS_"
+
+  test "Register derived":
+    let
+      port = samd21.getPeriphByName("PORT")
+      pmux1 = port.findRegisterByName("PMUX1_%s")
+      pmux0 = port.findRegisterByName("PMUX0_%s")
+
+    check:
+      pmux1.fields.len == pmux0.fields.len
+      pmux1.nimTypeName == pmux0.nimTypeName
+      pmux1.addressOffset == 0xb0
+
+      pmux1.properties.size == pmux0.properties.size
+      pmux1.properties.access == pmux0.properties.access
+
+suite "Dim Lists":
+  setup:
+    let
+      samd21Periphs {.used.} =
+        readSVD("./tests/ATSAMD21G18A.svd").peripherals.expandAllDimLists
+
+  test "Register dim list expanded":
+    let
+      ac = samd21Periphs.filterIt(it.name == "AC")[0]
+      compctrl = ac.registers.filterIt(it.name.contains("COMPCTRL"))
+
+    check:
+      compctrl.len == 2
+      compctrl[0].name == "COMPCTRL0"
+      compctrl[1].name == "COMPCTRL1"
+      compctrl[1].addressOffset - compctrl[0].addressOffset == 4
+      compctrl[0].description == compctrl[1].description
+      compctrl[0].fields.len == compctrl[1].fields.len
+      compctrl[0].nimTypeName == compctrl[1].nimTypeName
+      compctrl[0].properties.size == compctrl[1].properties.size
+      compctrl[0].properties.access == compctrl[1].properties.access
